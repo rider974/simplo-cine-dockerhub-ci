5c9ab2929ae17f714bff79dd9e5db1107be4d590
Thomas Legrand <pro.legrand.thomas@gmail.com>
2024-09-18 21:28:53 +0200

    fix users service

--- micro-services/users-service/controllers/userController.ts
+++ micro-services/users-service/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { UserService } from "../services/UserService";
+import { UserService } from "../services/userService";
 
 const userService = new UserService();
 
